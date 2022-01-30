4652dbfab648005e57595a01454e8af864d95493
Esma Suyolu <esmasuyolu@gmail.com>
2022-01-30 11:13:06 +0300

    Artık Yıl Hesaplayan Program

---
+++ src/Homework9.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Homework9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Yıl Giriniz: ");
+        int year = scanner.nextInt();
+
+        boolean isLeapYear = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+
+        if (isLeapYear) {
+            System.out.println(year + " bir artık yıldır!");
+        } else {
+            System.out.println(year + " bir artık yıl değildir!");
+        }
+    }
+}
