ae5af9ef658f69466e62750c6b5893ab33554167
Esma Suyolu <esmasuyolu@gmail.com>
2022-01-30 11:04:58 +0300

    Çin Zodyağı Hesaplayan Program

---
+++ src/Homework8.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Homework8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Doğum Yılınızı Giriniz: ");
+        int year = scanner.nextInt();
+        int sign = year % 12;
+        String horoscope = switch (sign) {
+            case 0 -> "Maymun";
+            case 1 -> "Horoz";
+            case 2 -> "Köpek";
+            case 3 -> "Domuz";
+            case 4 -> "Fare";
+            case 5 -> "Öküz";
+            case 6 -> "Kaplan";
+            case 7 -> "Tavşan";
+            case 8 -> "Ejderha";
+            case 9 -> "Yılan";
+            case 10 -> "At";
+            case 11 -> "Koyun";
+            default -> "";
+        };
+
+        System.out.print("Çin Zodyağı Burcunuz: " + horoscope);
+    }
+}
